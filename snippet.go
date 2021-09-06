2e9f37a37914747f6df8155a6e7f710e867a81be
zono <t.ishizono@futurek.com>
2021-09-06 18:53:41 +0900

    Update.

--- snippet.go
+++ snippet.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"time"
 )
 
 func IsError(err error) bool {
@@ -26,6 +27,7 @@ func Print() {
 	print("v2.0.1")
 }
 
+// IsExistKey は、マップに key が存在するかどうかを判定します。
 func IsExistKey(m map[interface{}]interface{}, key interface{}) (interface{}, bool) {
 	if val, ok := m[key]; ok {
 		return val, ok
@@ -34,6 +36,8 @@ func IsExistKey(m map[interface{}]interface{}, key interface{}) (interface{}, bo
 	}
 }
 
+// Mapper は、 構造体 org のプロパティを copy にマッピングします。
+//
 // 引数は構造体のアドレスを指定して、参照渡しとしてください。
 // 第一引数の構造体の値を第二引数の構造体にコピーします。
 // コピーするフィールド名は同じフィールド名のもののみです。
@@ -69,4 +73,28 @@ func Mapper(org, copy interface{}) {
 			reflect.ValueOf(copy).Elem().Field(barIndex).Set(fooValue)
 		}
 	}
+}
+
+// ToPtrS は、文字列を参照型で返します
+func ToPtrS(s string) *string {
+	return &s
+}
+
+// ToPtrI64 は、 Int64 を参照型で返します
+func ToPtrI64(i int64) *int64 {
+	return &i
+}
+
+// ToPtrTime は、文字列を time.Time 型に変換します
+//
+// layout 日付文字列のレイアウトを指定します
+// timeString 日付文字列
+func ToPtrTime(layout, timeString string) *time.Time {
+	if layout == "" {
+		layout = "2006/01/02 15:04.05"
+	}
+	nowTime, _ := time.Parse(layout, timeString)
+	// JST から UTC に
+	nowTime = nowTime.Add(-9 * time.Hour)
+	return &nowTime
 }
