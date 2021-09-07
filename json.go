cdad25f39feffa6c4676b9ebe30f04c22bb0e2ea
zono <t.ishizono@futurek.com>
2021-09-07 14:53:08 +0900

    Update go doc.

--- json.go
+++ json.go
@@ -2,11 +2,14 @@ package zonotools
 
 import "encoding/json"
 
+// DecodeJson は、 第一引数の interface 型を JSON 文字列の byte 配列に変換します。
+// data はポインター型でなければいけないです。
 func DecodeJson(data interface{}) ([]byte, error) {
 	return json.Marshal(data)
 }
 
-// Data required pointer
+// EncodeJson は、 第一引数の JSON 文字列を第二引数の interface 型に変換します。
+// data はポインター型でなければいけないです。
 func EncodeJson(jsonString string, data interface{}) error {
 	return json.Unmarshal([]byte(jsonString), data)
 }
